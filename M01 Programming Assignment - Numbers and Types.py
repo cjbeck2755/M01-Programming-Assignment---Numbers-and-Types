30743659891f8f7ca5949582154774f70075b22f
cjbeck2755 <cjbeck2755@yahoo.com>
2024-01-22 21:44:11 -0500

    Add files via upload

---
+++ M01 Programming Assignment - Numbers and Types.py
@@ -0,0 +1,22 @@
+#C.J. Becker
+#1/22/2024
+#Python 3.8.10
+#M01 Programming Assignment - Numbers and Types
+
+#3.1
+print(f"Number of seconds in an hour: {60*60}")
+
+#3.2
+seconds_per_hour = 60*60
+
+#3.3
+print(f"Number of seconds in a day: {seconds_per_hour*24}")
+
+#3.4
+seconds_per_day = seconds_per_hour*24
+
+#3.5
+print (f"{seconds_per_day / seconds_per_hour}")
+
+#3.6
+print (f"{seconds_per_day // seconds_per_hour}")
